f6b4b89ce6258a875d7f644ec4111c10234ee06e
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2021-01-20 18:58:10 +0530

    Initial Commit

---
+++ AppLock/AppLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLockApp.swift
+//  AppLock
+//
+//  Created by Azhagusundaram Tamil on 20/01/21.
+//
+
+import SwiftUI
+
+@main
+struct AppLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppLock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppLock
+//
+//  Created by Azhagusundaram Tamil on 20/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
